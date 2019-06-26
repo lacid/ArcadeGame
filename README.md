d439702df8d4ef707e34123b2dca6c9b6591b2b2
lacid <acidus.l@gmail.com>
2019-06-26 23:42:42 +0200

    class.alternative+readme

--- README.md
+++ README.md
@@ -1,8 +1,9 @@
-frontend-nanodegree-arcade-game
-===============================
+# frontend-nanodegree-arcade-game
 
-Students should use this [rubric](https://review.udacity.com/#!/projects/2696458597/rubric) for self-checking their submission. Make sure the functions you write are **object-oriented** - either class functions (like Player and Enemy) or class prototype functions such as Enemy.prototype.checkCollisions, and that the keyword 'this' is used appropriately within your class and class prototype functions to refer to the object the function is called upon. Also be sure that the **readme.md** file is updated with your instructions on both how to 1. Run and 2. Play your arcade game.
+Frogger kind of game, to experience OOP paradigm.
+Try the live version: [play here](https://lacid.github.io/ArcadeGame/) or download, unzip and open index.html using browser.
+Reach water to gain score.
 
-For detailed instructions on how to get started, check out this [guide](https://docs.google.com/document/d/1v01aScPjSWCCWQLIpFqvg3-vXLH2e8_SZQKC8jNO0Dc/pub?embedded=true).
+# TODO:
 
-Learn Canvas - https://eu.udacity.com/course/html5-canvas--ud292
+collectibles(gems..), improved score system(lives, bonus points, timer..), sounds, 2 player mode?
--- js/app.js
+++ js/app.js
@@ -40,9 +40,7 @@ const Player = function(x, y) {
   this.sprite = 'images/char-boy.png';
 };
 
-Player.prototype.update = function(dt) {
-  // ...
-};
+Player.prototype.update = function(dt) {};
 
 Player.prototype.render = function() {
   ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
@@ -61,15 +59,15 @@ Player.prototype.handleInput = function(input) {
   }
 
   if (this.y < 0) {
+    i++;
+    let counter = document.querySelector('#counter');
+    counter.innerHTML = `${i}`;
+
     // normal function wouldnt work because of .this
     setTimeout(() => {
       this.x = 101 * 2;
       this.y = 385;
     }, 300);
-
-    i++;
-    let counter = document.querySelector('#counter');
-    counter.innerHTML = `${i}`;
   }
 };
 
@@ -100,12 +98,46 @@ document.addEventListener('keyup', function(e) {
 
 // ********* class based alternative *********
 
+// class Enemy {
+//   constructor(x, y, move) {
+//     this.x = x;
+//     this.y = y;
+//     this.move = move;
+//     this.sprite = 'images/enemy-bug.png';
+//   }
+
+//   update = function(dt) {
+//     if (this.x < 505) {
+//       this.x += this.move * dt;
+//     } else {
+//       this.x = -101;
+//     }
+
+//     if (
+//       this.y === player.y &&
+//       (this.x - 55 < player.x && this.x > player.x - 55)
+//     ) {
+//       setTimeout(() => {
+//         player.x = 101 * 2;
+//         player.y = 385;
+//       }, 50);
+//     }
+//   };
+
+//   render = function() {
+//     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+//   };
+// }
+
+// let i = 0;
 // class Player {
-//   constructor() {
+//   constructor(x, y) {
 //     this.x = x;
 //     this.y = y;
 //     this.sprite = 'images/char-boy.png';
 //   }
+
+//   update = function(dt) {};
 
 //   render = function() {
 //     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
@@ -113,22 +145,50 @@ document.addEventListener('keyup', function(e) {
 
 //   handleInput = function(input) {
 //     if (input == 'left' && this.x > 0) {
-//   this.x -= 101;
-// } else if (input == 'right' && this.x < 404) {
-//   this.x += 101;
-// } else if (input == 'up' && this.y > 0) {
-//   this.y -= 83;
-// } else if (input == 'down' && this.y < 332) {
-//   this.y += 83;
-// }
+//       this.x -= 101;
+//     } else if (input == 'right' && this.x < 404) {
+//       this.x += 101;
+//     } else if (input == 'up' && this.y > 0) {
+//       this.y -= 83;
+//     } else if (input == 'down' && this.y < 303) {
+//       this.y += 83;
+//     }
 
-// if (this.y < 0) {
-//   // normal function wouldnt work because of this
-//   setTimeout(() => {
-//     this.x = 101 * 2;
-//     this.y = 385;
-//   }, 300);
-// }
+//     if (this.y < 0) {
+//       i++;
+//       let counter = document.querySelector('#counter');
+//       counter.innerHTML = `${i}`;
 
+//       // normal function wouldnt work because of .this
+//       setTimeout(() => {
+//         this.x = 101 * 2;
+//         this.y = 385;
+//       }, 300);
+//     }
 //   };
 // }
+
+// // instantiate objects; place all enemy objects in an array called allEnemies
+// const allEnemies = [];
+// const enemy1 = new Enemy(0, 53, 300);
+// const enemy2 = new Enemy(0, 136, 200);
+// const enemy3 = new Enemy(0, 219, 100);
+// allEnemies.push(enemy1, enemy2, enemy3);
+
+// // Place the player object in a variable called player
+// // as bottom plate creates pseudo 3d, had to pass Y manually
+// const player = new Player(101 * 2, 385);
+
+// // This listens for key presses and sends the keys to your
+// // Player.handleInput() method. You don't need to modify this.
+
+// document.addEventListener('keyup', function(e) {
+//   var allowedKeys = {
+//     37: 'left',
+//     38: 'up',
+//     39: 'right',
+//     40: 'down'
+//   };
+
+//   player.handleInput(allowedKeys[e.keyCode]);
+// });
